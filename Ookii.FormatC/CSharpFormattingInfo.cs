77030402067a7eecee1fc73f9ca5decf995886d9
Sven Groot <svgroot@microsoft.com>
2021-05-05 16:13:21 -0700

    Updated C# keywords for C# 9.0.

--- Ookii.FormatC/CSharpFormattingInfo.cs
+++ Ookii.FormatC/CSharpFormattingInfo.cs
@@ -66,14 +66,15 @@ namespace Ookii.FormatC
                         new CodeElement("escapedIdentifier", @"@[a-zA-Z0-9_]+") { ElementNameIsCssClass = false },
                         new CodeElement("keyword", new string[] { "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char",
                             "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event",
-                            "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "in",
-                            "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "out",
+                            "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in",
+                            "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out",
                             "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof",
                             "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong",
                             "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while",
                             // The remainder are contextual keywords
-                            "add", "dynamic", "from", "get", "global", "group", "into", "join", "let", "orderby", "partial", "remove", "select",
-                            "set", "value", "var", "where", "yield"
+                            "add", "and", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get", "global",
+                            "group", "init", "into", "join", "let", "managed", "nameof", "nint", "not", "notnull", "nuint", "on", "or", "orderby",
+                            "partial", "record", "remove", "select", "set", "unmanaged", "value", "var", "when", "where", "with", "yield"
                         }),
                         new CodeElement("preprocessor", new string[] { "#if", "#else", "#elif", "#endif", "#define", "#undef", "#warning", "#error", 
                         "#line", "#region", "#endregion", "#pragma" }) 
